b94a592210f0015226edee69c7eea6db2ae347cf
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-12 16:47:45 +1000

    Project config CI integration Xcode server Xcode 10 new build system bugs

---
+++ Scripts/buildVersionIncrementer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#  Script.sh
+#  TDDPlaces
+#
+#  Created by SLim on 12/12/18.
+#  Copyright © 2018 SLim. All rights reserved.
+#!/bin/bash
+branch=${1:-'master'}
+buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
+echo "Updating build number to $buildNumber using branch '$branch'."
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+if [ -f "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist" ]; then
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist"
+fi
+
+#buildNumber=$(git rev-list --count HEAD)
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$PRODUCT_SETTINGS_PATH"
